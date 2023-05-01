17136469b6ebe5fc9cf6a773dd5ea8b9a1c6c382
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2023-04-30 23:53:58 -0400

    write to curosr pos

--- python3/poetry_vim/poetry.py
+++ python3/poetry_vim/poetry.py
@@ -3,4 +3,13 @@ import vim
 class Poet:
     def test(self) -> None:
         print("hello there!")
-        vim.current.buffer.append("hello there!")
+        row, col = vim.current.window.cursor
+        buffer = vim.current.buffer
+        print(buffer, row, col)
+
+        buffer[row-1] = buffer[row-1][:col] + "hello there!" + buffer[row-1][col:]
+
+        new_col = col + len("hello there!")
+        vim.current.window.cursor = (row, new_col)
+
+
