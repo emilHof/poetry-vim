017337d7b3583316a8eafaa8757c574b2e2491c2
Emil Hofstetter <emil.k.hofstetter@gmail.com>
2023-05-01 00:12:04 -0400

    write to curosr pos

--- python3/poetry_vim/poetry.py
+++ python3/poetry_vim/poetry.py
@@ -1,4 +1,11 @@
 import vim
+import requests
+import random
+
+class Poem:
+    def __init__(self, title: str ,lines: list[str]) -> None:
+        self.title = title
+        self.lines = lines
 
 class Poet:
     def test(self) -> None:
@@ -12,5 +19,27 @@ class Poet:
 
         new_col = col + len("hello there!")
         vim.current.window.cursor = (row, new_col)
+
+    def _get_poetry(self) -> str:
+        author = self._get_rand_author()
+        print(f"author: {author}")
+        poem = self._get_poem(author)
+        print(f"poem: {poem}")
+        return "\n".join(poem.lines[:min(2, len(poem.lines))])
+    
+    def _get_rand_author(self) -> str:
+        body = requests.get("https://poetrydb.org/author").json()
+
+        authors = body.authors
+        return authors[random.randrange(0, len(authors))]
+    
+    def _get_poem(self, author: str) -> Poem:
+        poems: list[Poem] = requests.get("https://poetrydb.org/author/{author}/title,lines").json()
+        return poems[random.randrange(0, len(poems))]
+        
+
+
+
+
 
 
